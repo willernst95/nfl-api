7e24c87864c335e5af9d39634740cde16e2b40cf
willernst95 <ernstwill4224@gmail.com>
2021-02-25 09:18:10 -0600

    changes

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = (props) => {
+    return (
+        <div class = "text-center">Coming from a 3x Champion</div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = (props) => {
+    return (
+        <div class = "text-center">NFL Fantasy Project</div>
+    )
+}
+
+export default Header;
